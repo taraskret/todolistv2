bba2afc5ee6263e5ebc8e88bb7194322af86afa9
taraKret <melaus.edu@gmail.com>
2021-06-24 12:18:56 +1000

    add gitignore, procfile, updates ports

--- app.js
+++ app.js
@@ -130,6 +130,10 @@ res.render('list',{listTitle: foundList.name, newListItems: foundList.items} )
 } )
 })
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, ()=>{ 
+  console.log("Server started on port 3000")
 });
